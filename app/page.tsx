5dcbcc79d67ce91cccddd8d657ed80970aef99c9
Leo Sun <zhanpeng.sun899@gmail.com>
2025-06-14 19:33:24 +0930

    changed page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <>
         <h1>App Router1</h1>
-        <h2 style={{ textAlign: "center", color: "red" }}>Changed something</h2>
+        <h2 style={{ textAlign: "center", color: "red" }}>Changed something on 140625 19:33 </h2>
     </>
   );
 }
